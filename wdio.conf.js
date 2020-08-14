09959b4ba52c7fc69bd47e15640260725ac7aafa
miasayedau <61624338+miasayedau@users.noreply.github.com>
2020-08-14 18:01:54 +0300

    Add object declaration in before method

--- wdio.conf.js
+++ wdio.conf.js
@@ -241,7 +241,7 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that are to be run
      */
-    before: (capabilities, specs) => {
+    before: function (capabilities, specs) {
         browser.pageObjects = {
             'Главная страница': require('./tests/pages/mainPage'),
 
