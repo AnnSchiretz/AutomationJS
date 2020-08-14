aec0dfae9b7506b37d0be3fa69a7b74a9d7a3eec
miasayedau <61624338+miasayedau@users.noreply.github.com>
2020-08-14 17:41:41 +0300

    Update data in valueData file

---
+++ tests/files/testData/valueData.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'Правильная почта': 'e.miasoedov+test_9999@jetmail.cc',
+    'Правильный пароль': 'password_1',
+
+    //SOL
+    'Правильный тайтл SOL Sport': "Сол Казино (Sol Casino) официальный сайт c бонусом за регистрацию",
+
+    //JET
+    'Правильный тайтл JET Bonuses': "Jet Casino is an official website to play slot machines online with a bonus for registration - Jet.casino"
+
+};
+
