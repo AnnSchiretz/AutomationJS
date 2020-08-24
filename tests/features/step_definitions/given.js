28821da8ee1836e6b150a69adf0d0d757cf52a69
miasayedau <61624338+miasayedau@users.noreply.github.com>
2020-08-24 17:08:18 +0300

    Update step-definitions files

--- tests/features/step_definitions/given.js
+++ tests/features/step_definitions/given.js
@@ -5,3 +5,8 @@ Given(/^Я нахожусь на главной странице$/, () => {
      browser.url(process.env.URL);
 
 });
+
+Given(/^Я нахожусь на лендинге$/, () => {
+     browser.url(process.env.URL_LAND);
+
+});
--- tests/features/step_definitions/when.js
+++ tests/features/step_definitions/when.js
@@ -2,6 +2,7 @@ const {When} = require('cucumber');
 const elementManager = require('../../services/elementManager');
 const dataTest = require('../../files/testData/valueData');
 
+
 When(/^Я нажимаю "([^"]*)" на "([^"]*)"$/, (element, pageObject) => {
 
     const component = browser.pageObjects[pageObject].elements[element];
@@ -12,23 +13,27 @@ When(/^Я нажимаю "([^"]*)" на "([^"]*)"$/, (element, pageObject) => {
 When(/^Я ввожу "([^"]*)" в "([^"]*)" на "([^"]*)"$/, (value, element, pageObject) => {
 
     const component = browser.pageObjects[pageObject].elements[element];
-    elementManager.setValue(component, dataTest[value]);
+    return elementManager.setValue(component, dataTest[value]);
 
 });
 
 // Действия с элементами из Header
-When(/^Я нажимаю "([^"]*)" в хедере "([^"]*)"$/, (element, project)=> {
+When(/^Я нажимаю "([^"]*)" в хедере$/, (element)=> {
 
-
-    const component = browser.headers[project].elements[element];
+    const component = browser.headers[process.env.PROJECT].elements[element];
     elementManager.click(component);
 
 });
 
 // Действия с элементами из Sidebar
-When(/^Я нажимаю "([^"]*)" в боковом меню "([^"]*)"$/, (element, project)=> {
+When(/^Я нажимаю "([^"]*)" в боковом меню$/, (element)=> {
 
-    const component = browser.sidebars[project].elements[element];
+    const component = browser.sidebars[process.env.PROJECT].elements[element];
     elementManager.click(component);
+
+});
+When(/^Я зарегистрирован как пользователь$/, ()=> {
+
+
 
 });
