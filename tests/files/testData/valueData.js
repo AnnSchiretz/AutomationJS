202e013c164d255167e48901af09614e63cb7070
miasayedau <61624338+miasayedau@users.noreply.github.com>
2020-08-24 17:15:20 +0300

    Add new test data

--- tests/files/testData/valueData.js
+++ tests/files/testData/valueData.js
@@ -1,6 +1,18 @@
 module.exports = {
     'Правильная почта': 'e.miasoedov+test_9999@jetmail.cc',
     'Правильный пароль': 'password_1',
+    'Пароль Новорег': '123123123',
+    'Случайная почта': ("autotest+"+browser.helper.randomNumber+browser.helper.randomNumber+"@jetmail.cc"),
+    'Логин VK': "+79587998611",
+    'Пароль VK': "qwerty!123456",
+    'Регистрация ОК': (process.env.URL+"users/auth/odnoklassniki"),
+    'Регистрация Facebook': (process.env.URL+"users/auth/facebook"),
+    'Регистрация VK': (process.env.URL+"users/auth/vkontakte"),
+    'Регистрация MailRU': (process.env.URL+"users/auth/mailru"),
+    'Регистрация Yandex': (process.env.URL+"users/auth/yandex"),
+    'Регистрация Google+': (process.env.URL+"users/auth/google_oauth2"),
+    'Регистрация Telegram': (process.env.URL+"users/auth/tlgrm"),
+
 
     //SOL
     'Правильный тайтл SOL Sport': "Сол Казино (Sol Casino) официальный сайт c бонусом за регистрацию",
@@ -9,4 +21,3 @@ module.exports = {
     'Правильный тайтл JET Bonuses': "Jet Casino is an official website to play slot machines online with a bonus for registration - Jet.casino"
 
 };
-
