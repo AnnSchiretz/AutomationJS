14b493399e7dc7f15f06ecbb2914302f9a531ca2
miasayedau <61624338+miasayedau@users.noreply.github.com>
2020-09-10 13:46:45 +0300

    Add the function of generating a random phone number

--- tests/helper/helper.js
+++ tests/helper/helper.js
@@ -2,12 +2,20 @@ class Helper {
 
 
     get randomEmail() {
-        {
-            const randomNumber = Math.floor(Math.random() * 9999);
-            return ("autotest+" + randomNumber + randomNumber + "@jetmail.cc");
-        }
+
+        const randomNumber = Math.floor(Math.random() * 9999);
+        return ("autotest+" + randomNumber + randomNumber + "@jetmail.cc");
+
     }
 
+
+    get randomPhoneNumber() {
+
+        const randomNumber = Math.floor(Math.random() * 9999);
+        return (`91${randomNumber}${randomNumber}`);
+
+    }
 }
+
 
 module.exports = new Helper();
