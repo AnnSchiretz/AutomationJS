c521b365abee627f26c56906594c7fad516164a1
miasayedau <61624338+miasayedau@users.noreply.github.com>
2020-09-23 13:22:06 +0300

    Add mobile configs for mobile testing

---
+++ tests/config/androidBrowser.config.js
@@ -0,0 +1,26 @@
+const {config} = require('./../../wdio.conf');
+
+//Samsung s8
+config.capabilities = [
+    {
+        platformName: 'Android',
+        browserName: 'chrome',
+        maxInstances: 1,
+        automationName: 'uiautomator2',
+        deviceName: 'ce031713655bc80d0c',//emulator-5554    ce031713655bc80d0c
+        platformVersion: '',
+        port: 4723,
+        path: '/wd/hub',
+        appium: {
+            // For options see
+            // https://github.com/webdriverio/webdriverio/tree/master/packages/wdio-appium-service
+            args: {},
+            // command: 'appium',
+        },
+    },
+
+];
+config.services = [];
+
+
+exports.config = config;
---
+++ tests/config/iOSBrowser.js
@@ -0,0 +1,40 @@
+const {config} = require('./../../wdio.conf');
+
+
+config.capabilities = [
+
+    {
+//
+//         // udid: "D17C630D-57E5-44FF-A2E0-B2DB40697DAC",    //эмулятор
+//         // udid: "0A610A2A-2A23-44B3-B2A7-1",//11 iPhone
+//         // udid: "00008020-001465CA1488003A",
+//         UDID: "4255f12e8bf82e540ed8ef0bfa34ea88e98ddee1",
+//
+
+        platformName: 'iOS',
+        browserName: 'safari',
+        maxInstances: 1,
+        automationName: 'XCUITest',
+        udid: "D17C630D-57E5-44FF-A2E0-B2DB40697DAC",
+        deviceName: 'iPhone 7',
+        platformVersion: '13.1.1',
+        // xcodeOrgId: 'UNIONSTAR LIMITED',
+        // xcodeSigningId: 'Marina Fedko (NH75D89LKU)',
+        // bundleIdentifier: "TEST.TEST",
+        port: 4723,
+        path: '/wd/hub',
+        appium: {
+            // For options see
+            // https://github.com/webdriverio/webdriverio/tree/master/packages/wdio-appium-service
+            args: {},
+            // command: 'appium',
+        },
+    }
+//Marina Fedko (NH75D89LKU)
+
+
+];
+config.services = [];
+
+
+exports.config = config;
